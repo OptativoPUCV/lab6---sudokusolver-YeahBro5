a92381670ea6b5f2bfbeb27d5549ede2771af209
Your Name <you@example.com>
2024-05-11 20:32:38 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -120,6 +120,7 @@ List* get_adj_nodes(Node* n)
                         pushBack(list, newNode);
                         
                      }
+                  return list;
                }
                
             }
